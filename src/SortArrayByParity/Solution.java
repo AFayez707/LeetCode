0c1716ecb97952d94d27cf68f3c74dfa62349476
Ahmed Fayez <a.fayez707@gmail.com>
2022-04-06 14:35:33 -0500

    New Solutions.

---
+++ src/SortArrayByParity/Solution.java
@@ -0,0 +1,18 @@
+package SortArrayByParity;
+
+public class Solution {
+//    url: https://leetcode.com/problems/sort-array-by-parity/solution/
+    public int[] sortArrayByParity(int[] nums) {
+        int leftPointer = 0, rightPointer = nums.length - 1;
+        while (leftPointer != rightPointer) {
+            if(nums[leftPointer] % 2 == 0) {
+                leftPointer++;
+            } else {
+                int temp = nums[leftPointer];
+                nums[leftPointer] = nums[rightPointer];
+                nums[rightPointer--] = temp;
+            }
+        }
+        return nums;
+    }
+}
---
+++ src/ThirdMaximumNumber/Solution.java
@@ -0,0 +1,21 @@
+package ThirdMaximumNumber;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Set;
+
+public class Solution {
+    public int thirdMax(int[] nums) {
+        Set<Integer> maximumsSet = new HashSet<>();
+
+        for(int num : nums) {
+            maximumsSet.add(num);
+            if(maximumsSet.size() > 3) {
+                maximumsSet.remove(Collections.min(maximumsSet));
+            }
+        }
+
+        return maximumsSet.size() == 3 ? Collections.min(maximumsSet) : Collections.max(maximumsSet);
+    }
+}
